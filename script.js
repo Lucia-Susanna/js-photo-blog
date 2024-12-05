ca74b30176cd2358339f41ed40aba38b42de1032
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-12-05 12:10:47 +0100

    feat: milestone 2 (previsione del lavoro in commenti nel file js)

---
+++ script.js
@@ -0,0 +1,4 @@
+// prendo gli elementi che mi serviranno: la card in cui andrò a inserire l'html, la img della card e la caption
+//l'api in questo caso serve a generare un numero di immagini con descrizione pari a quanto indicato in _limit 
+// mi restituisce un json in cui mi rervono questi elementi: title e url
+// salvando questi due elementi in una costante vado a inserirli man mano (con unn ciclo) nell'html della card concatenandoli
