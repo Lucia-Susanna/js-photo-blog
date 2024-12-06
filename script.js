85dc3c3e08670f55e35348b2daea6eba5831cd7b
Lucia-Susanna <lucia_susanna@hotmail.it>
2024-12-06 19:36:46 +0100

    feat: day 2 milestone 2

--- script.js
+++ script.js
@@ -6,34 +6,54 @@
 
 // richiamo i nodi dal document
 const row = document.querySelector('.row');
-const cardNumber = 9
+const cardNumber = 6
 
 //salvo l'endpoint
 const endpoint = 'https://jsonplaceholder.typicode.com/photos?_limit=' + cardNumber;
 
 //faccio la chiamata all'api
 axios.get(endpoint)
-  .then(response =>{
-    const photosArray = response.data
-    console.log(photosArray)
-    photosArray.forEach(photo => {      
-      const {title, url} = photo
-      row.innerHTML += `<div class="column d-flex center debug">
-         <div class="card relative debug">
-           <img class="pin absolute" src="./assets_day1/img/pin.svg" alt="">
-           <div class="img">
-             <img id="photo" src="${url}" alt="img">
-           </div>
-           <div class="caption">
-             <p>${title}</p>
-           </div>
-         </div>
-        </div>        `
-    });
-    
+.then(response =>{
+  const photosArray = response.data
+  
+  photosArray.forEach(photo => {    
+    const {title, url} = photo
+    row.innerHTML  += `<div class="column d-flex center debug">
+    <div class="card relative debug">
+    <img class="pin absolute" src="./assets_day1/img/pin.svg" alt="">
+    <div class="img">
+    <img src="${url}" alt="img">
+    </div> 
+    <div class="caption">
+    <p>${title}</p>
+    </div>
+    </div>
+    </div>`
   })
-  .catch(e =>
-    console.log(e)
-  )
+  
+  // seleziono nel codice html tutti i tag con classe img
+  const images = document.querySelectorAll('.img')
+
+  // per ogni tag selezionato aggiungo l'evento che mostra l'overlay
+  images.forEach(image =>{
+    image.addEventListener('click', () => overlay.classList.remove('hide'))  
+})
+
+})
+.catch(e =>
+  console.log(e)
+)
+
+// al click del bottone aggiungo la classe hide all'overlayer
+// al click di una qualsiasi card tolgo la classe hide
+
+const btn = document.querySelector('.close')
+const overlay = document.querySelector('.overlay')
+
+
+btn.addEventListener('click', () => overlay.classList.add('hide'))
+
+
+
 
 
